3151985f97a060c03dfaa06d02d5fb2677403e00
Eduardo Gonzalo Espinoza Carreon <eduardoespc@gmail.com>
2015-03-31 15:06:34 -0500

    updating description for split_bill

--- python_test/test.py
+++ python_test/test.py
@@ -8,14 +8,16 @@ def split_bill(price, discount, people):
     """
     divide la cuenta de una mesa
     :param price: precio a dividir
-    :param discount: descuento, si no hay descuento el valor es 0
+    :param discount: descuento, si no hay descuento el valor es 0. el valor representa el
+    porcentaje [0,100]
     :param people: array con numeros de la parte que le corresponde a cada persona
      Ej:
      - si dividen 1 plato entre 3 seria [(1/3), (1/3), (1/3)]
      - tambien pueden existir divisiones desiguales: 3 personas pero 1 de ellas paga la mitad
         [(1/2), (1/4), (1/4)]
     :return: array con el monto a pagar para cada persona
-    Ej: monto S/. 20 y people = [(1/2), (1/2)] => [10, 10]
+    Ej:
+    monto S/. 20 y people = [(1/2), (1/2)] => [10, 10]
     """
     raise NotImplementedError
 
